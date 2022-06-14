7cba859657a85d19615558b1561acbc37a5ec92f
MinJunKim21 <jinju8012@gmail.com>
2022-06-14 17:40:04 +0900

    starting router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import { useState, useEffect } from "react";
+import Movie from './components/Movie';
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const getMovies = async () => {
-    const json = await (
-      await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.1&sort_by=year')
-    ).json();
-    setMovies(json.data.movies)
-    setLoading(false)
-  }
-  useEffect(() => {
-    getMovies()
-  }, [])
-  // useEffect(() => {
-  //   fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')
-  //   .then(res => res.json())
-  //   .then(json=> {
-  //     setMovies(json.data.movies)
-  //     setLoading(false)})
-  // },[])
-  
-  return (
-    <div className="App">
-      {loading? <h1>Loading...</h1> : 
-      <div>
-        {movies.map(movie=> 
-        <div key={movie.id}>
-          <img src={movie.medium_cover_image} />
-          <h2>{movie.title}</h2>
-          <p>{movie.summary}</p>
-          <ul>
-            {movie.genres.map(g => <li key={g}>{g}</li>)}
-          </ul>
-        </div>
-        )}
-      </div>}
-    </div>
-  );
+  return null
 }
 
 export default App;
+ 
